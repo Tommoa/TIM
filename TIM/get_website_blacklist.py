fef92677ee124b33d652b1d10f125b1f3f2b4ca4
khuang247 <31398253+khuang247@users.noreply.github.com>
2020-03-13 14:23:03 +0800

    Add search string

--- TIM/get_website_blacklist.py
+++ TIM/get_website_blacklist.py
@@ -19,4 +19,42 @@ bp = Blueprint('get_website_blacklist', __name__, url_prefix='/get_website_black
 @cross_origin()
 @login.token_required
 def get_website_blacklist():
-    return "Website Blacklist"
+	# Set up config
+    HOST = app.config['HOST']
+    PORT = app.config['PORT']
+    USERNAME = app.config['USERNAME']
+    PASSWORD = app.config['PASSWORD']
+
+    service = client.connect(
+        host=HOST,
+        port=PORT,
+        username=USERNAME,
+        password=PASSWORD)
+
+    search_time = '-3mon'
+    exec_mode = {"exec_mode": "normal"}
+
+    # Add in time ranges later
+    # Need to connect to IDs later
+    # | lookup otherlog | ip as src_ip OUTPUT id
+    # etc.
+    search_string = '''
+    * "Built inbound * connection" earliest={}
+	| rex "for\s.*?:(?<src_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+	| rex "to internet:(?<dest_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+	| search
+	    [| inputlookup myblacklist_lookup
+	    | rename _key as dest_ip
+	    | fields dest_ip]
+	| table _time, src_ip, dest_ip
+    '''.format(search_time)
+
+    job = service.jobs.create(search_string, **exec_mode)
+
+    # Wait until search is completed
+    while True:
+        while not job.is_ready():
+            pass
+        if(job["isDone"]):
+            break
+        sleep(2)
