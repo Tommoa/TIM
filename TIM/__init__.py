ef3ce8ebc08964cf565c8b8c50c26026d7fe6115
Dylan Bell <dylan-bell2@hotmail.com>
2020-03-27 01:58:21 +0800

    Add database class
    Still a wip but initial database class using tinyDB

--- TIM/__init__.py
+++ TIM/__init__.py
@@ -27,6 +27,7 @@ def create_app(test_config=None):
     # register blueprints e.g. endpoints
     from . import get_id, get_mac
     from . import get_email_header, get_website_blacklist, get_brute_force
+
     app.register_blueprint(get_id.bp)
     app.register_blueprint(get_mac.bp)
     app.register_blueprint(get_website_blacklist.bp)
--- TIM/get_brute_force.py
+++ TIM/get_brute_force.py
@@ -4,6 +4,7 @@ from time import sleep
 import splunklib.results as results
 import splunklib.client as client
 
+
 from flask import (
     Blueprint, jsonify, flash, g, redirect, render_template, request, session, url_for
 )
@@ -14,4 +15,11 @@ bp = Blueprint('get_brute_force', __name__, url_prefix='/get_brute_force')
 
 @bp.route('/')
 def get_brute_force():
+
+    # How to open a table and insert a row.
+    from . import database
+    db = database.db()
+    db.blacklist_table.insert({'id': 12345678, 'time': '09/02/20 12:30', 'message': '20 failed log in attempts'})
+
+
     return "Brute Force"
