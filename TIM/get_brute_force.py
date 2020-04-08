e66b783273565a3d322993359175833fa895c2e1
Alphons <alphonsg@protonmail.com>
2020-04-08 14:12:22 +0800

    Remove unneeded import

--- TIM/get_brute_force.py
+++ TIM/get_brute_force.py
@@ -4,7 +4,6 @@ from time import sleep
 import splunklib.results as results
 import splunklib.client as client
 from . import database
-import schedule
 import tinydb
 
 
@@ -18,9 +17,9 @@ bp = Blueprint('get_brute_force', __name__, url_prefix='/get_brute_force')
 
 @bp.route('/')
 def get_brute_force():
-    
+
     db = database.db()
     db.blacklist_table.insert({'id': 12345678, 'time': '09/02/20 12:30', 'threat': 'low', 'message': '20 failed log in attempts'})
     print (db.blacklist_table.search(tinydb.where('id') == 12345678))
-    
+
     return "Hello"
