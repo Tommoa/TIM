dd63df80fed30ec012b9f8c666b51f0710ac1126
Alphons <alphonsg@protonmail.com>
2020-04-10 18:38:39 +0800

    Update query string + bug-fixes

--- TIM/multi_logins.py
+++ TIM/multi_logins.py
@@ -7,7 +7,7 @@ def gen_multi_logins_query():
     unique_logins_thresh = "1"
 
     # Generate other necessary parameters for search
-    exec_mode = {"exec_mode": "normal"}
+    threat_name = "multi_logins"
     # Get number representing window width from time_window spl arg
     match = search(r'\d+', time_window)
     # TODO: Process and check user inputs externally, with exceptions.
@@ -18,20 +18,23 @@ def gen_multi_logins_query():
                 time_window)
         raise ValueError(msg)
 
-    search_string = """
-        search * is-ise (cise_passed_authentications
-        AND RadiusFlowType=Wireless802_1x)
-        | sort 0 _time
-        | bin _time span={}
-        | stats dc(UserName) AS unique_logins, values(UserName) AS usernames
-        BY _time EndPointMACAddress
-        | streamstats time_window=5m min(_time) AS start,  max(_time) AS end,
-        sum(unique_logins) AS logins BY EndPointMACAddress
-        | eval _time = start, end = start + {}
-        | where unique_logins >= {}
-        | stats values(start) AS start_times, values(end) AS end_times,
-        values(usernames) AS usernames, values(unique_logins) AS unique_logins
-        BY EndPointMACAddress
-    """.format(time_window, delta_t, unique_logins_thresh)
+    search_query = """
+            search * is-ise (cise_passed_authentications
+            AND RadiusFlowType=Wireless802_1x)
+            | sort 0 _time
+            | bin _time span={}
+            | stats dc(UserName) AS unique_logins, values(UserName) AS username
+            BY _time EndPointMACAddress
+            | streamstats time_window={} min(_time) AS start,
+            max(_time) AS end, sum(unique_logins) AS unique_logins
+            BY EndPointMACAddress
+            | eval _time = start, end = start + {}, threat = "{}"
+            | where unique_logins >= {}
+            | rename EndPointMACAddress AS mac
+            | stats list(threat) AS threat, list(start) AS time,
+             list(unique_logins) AS unique_logins, list(username) AS username
+             BY mac
+            """.format(time_window, time_window, delta_t, threat_name,
+            unique_logins_thresh)
 
-    return search_string
+    return search_query
